f70aa5965a09e59e4d0e225af2a8d58ef93ed41f
Nipo Cyrus <118452258+Nipo-Cyrus@users.noreply.github.com>
2023-03-16 00:46:59 +0000

    Form Progress: 64%

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Random-Project
-Creativity is the Limit
+This Repository is only for Writer's Random Thoughts and Coding program that have been expereminted on
+
+This Repositroy does not contain any activities related to the User's Acitvitie's some may contain codes that have been used on ceertain Performance Tasks  but does not conclude that it can be any help to your Expectations
+
+This Repository is purely experimnental
---
+++ php/test1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style text/css>
+    .table1 {
+        width: 400px;
+    }
+
+    </style>
+    <title>Document</title>
+</head>
+<body>
+     <form action="" method="post">
+        <table class="table1" border="1">
+            <tr>
+                <th>Login</th>
+            </tr>
+            <tr> <td>
+                <table border=1 class="table2">
+                    <tr>
+                        <td>Username <br> <input type="text" name="username" id="username"></td>
+                    </tr>
+                    <tr>
+                        <td>Password <br> <input type="password" name="password" id="password"></td>
+                    </tr>
+                </table></td>
+            </tr>
+        </table>
+      
+
+     </form>
+</body>
+</html>
